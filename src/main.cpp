885477bd422e60ff360aea10d4b598111f1ac78b
Guillaume Lefrant <lefrantguillaume@gmail.com>
2016-04-05 01:20:31 +0100

    Updated CMake

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	srand(time(NULL));
-	std::cout << "Version " << asteroidzz_VERSION_MAJOR << "." << asteroidzz_VERSION_MINOR << std::endl;
+	std::cout << "Version " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
 	Game *g = new Game();
 	g->loop();
 	delete g;
